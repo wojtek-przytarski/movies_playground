3912874e09d3fd83a924bdf5375cc12d39598cf6
wojtek.przytarski <vojtek16@gmail.com>
2019-03-17 12:29:59 +0100

    Moved add_movie to another class

---
+++ api/externalapihandler/movie_proxy.py
@@ -0,0 +1,68 @@
+from api.models import Rating, Genre, Actor, Movie
+
+
+class MovieProxy:
+
+    def __init__(self, api):
+        self.omdb_api = api
+
+    def get_movie_details(self, title):
+        try:
+            return Movie.objects.get(title=title)
+        except Movie.DoesNotExist:
+            return self._add_new_movie(title)
+
+    def _add_new_movie(self, title):
+        omdb_result = self.omdb_api.get_movie(title)
+        movie_args = {k[:1].lower() + k[1:]: v for k, v in omdb_result.items()}
+        movie = self._add_movie_from_omdb_result(movie_args)
+        return movie
+
+    def _add_movie_from_omdb_result(self, movie_args):
+        foreign_key_values = {'actors': self._get_actor_set(movie_args.get('actors')),
+                              'genre': self._get_genre_set(movie_args.get('genre')),
+                              'ratings': self._get_rating_set(movie_args.get('ratings'))}
+        movie = Movie()
+        for key, value in movie_args.items():
+            action = foreign_key_values.get(key)
+            if not action:
+                movie.__setattr__(key, value)
+        movie.save()  # object must be saved to add values to manytomany fields
+        for key, values_set in foreign_key_values.items():  # so add it after saving
+            movie.__getattribute__(key).add(*values_set)
+        movie.save()
+        return movie
+
+    @staticmethod
+    def _get_actor_set(omdb_actor):
+        actor_set = set()
+        for actor_name in omdb_actor.split(', '):
+            try:
+                actor_set.add(Actor.objects.get(name__icontains=actor_name))
+            except Actor.DoesNotExist:
+                a = Actor(name=actor_name)
+                a.save()
+                actor_set.add(a)
+        return actor_set
+
+    @staticmethod
+    def _get_genre_set(omdb_genre):
+        genre_set = set()
+        for genre_name in omdb_genre.split(', '):
+            try:
+                genre_set.add(Genre.objects.get(name__icontains=genre_name))
+            except Genre.DoesNotExist:
+                g = Genre(name=genre_name)
+                g.save()
+                genre_set.add(g)
+        return genre_set
+
+    @staticmethod
+    def _get_rating_set(omdb_ratings):
+        rating_set = set()
+        for rating in omdb_ratings:
+            r = Rating(source=rating.get('Source'), value=rating.get('Value'))
+            r.save()
+            rating_set.add(r)
+            r.save()
+        return rating_set
--- api/views.py
+++ api/views.py
@@ -1,16 +1,14 @@
 from venv import logger
 
-from django.core import serializers
 from django.http import JsonResponse
 from rest_framework import viewsets
 
 from api.exceptions import OmdbApiResponseException
+from api.externalapihandler.movie_proxy import MovieProxy
 from api.externalapihandler.omdb_api_handler import OmdbApiHandler
 from api.models import Movie, Actor, Genre, Rating
 from api.serializers import MovieSerializer, GenreSerializer, ActorSerializer
 from movies_playground.settings import OMDB_API_KEY
-
-omdb_api = OmdbApiHandler(OMDB_API_KEY)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -27,75 +25,19 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    def __init__(self, **kwargs):
+        self.movie_proxy = MovieProxy(OmdbApiHandler(OMDB_API_KEY))
+        super().__init__(**kwargs)
+
     def create(self, request, **kwargs):
         movie_title = request.data.get('title')
         if not movie_title:
             return JsonResponse({'Error': 'Invalid request. Please provide \'title\' in request.'})
         try:
-            return JsonResponse(
-                MovieSerializer(Movie.objects.get(title=movie_title), context={'request': request}).data)
-        except Movie.DoesNotExist:
-            try:
-                movie = self._add_new_movie(movie_title)
-                return JsonResponse(MovieSerializer(movie, context={'request': request}).data)
-            except OmdbApiResponseException as ex:
-                logger.error(ex)
-                return JsonResponse({'Error': 'Invalid movie title.'})
-
-    def _add_new_movie(self, movie_title):
-        movie_info = omdb_api.get_movie(movie_title)
-        movie = self._get_movie_from_omdb_result(movie_info)
-        return movie
-
-    def _get_movie_from_omdb_result(self, omdb_result):
-        # TODO: clean this mess and maybe move it to another place
-        movie_args = {k[:1].lower() + k[1:]: v for k, v in omdb_result.items()}  # change keys to make it same as model
-        foreign_key_action = {'actors': self._get_actor_set,
-                              'genre': self._get_genre_set,
-                              'ratings': self._get_rating_set}
-        movie = Movie()
-        for key, value in movie_args.items():
-            action = foreign_key_action.get(key)
-            if not action:
-                movie.__setattr__(key, value)
-        movie.save()  # object must be saved to add to manytomany fields
-        for key, action in foreign_key_action.items():
-            values_set = action(movie_args.get(key))
-            for value in values_set:
-                movie.__getattribute__(key).add(value)
-        movie.save()
-        return movie
-
-    @staticmethod
-    def _get_actor_set(omdb_actor):
-        actor_set = set()
-        for actor_name in omdb_actor.split(', '):
-            try:
-                actor_set.add(Actor.objects.get(name__icontains=actor_name))
-            except Actor.DoesNotExist:
-                a = Actor(name=actor_name)
-                a.save()
-                actor_set.add(a)
-        return actor_set
-
-    @staticmethod
-    def _get_genre_set(omdb_genre):
-        genre_set = set()
-        for genre_name in omdb_genre.split(', '):
-            try:
-                genre_set.add(Genre.objects.get(name__icontains=genre_name))
-            except Genre.DoesNotExist:
-                g = Genre(name=genre_name)
-                g.save()
-                genre_set.add(g)
-        return genre_set
-
-    @staticmethod
-    def _get_rating_set(omdb_ratings):
-        rating_set = set()
-        for rating in omdb_ratings:
-            r = Rating(source=rating.get('Source'), value=rating.get('Value'))
-            r.save()
-            rating_set.add(r)
-            r.save()
-        return rating_set
+            movie = self.movie_proxy.get_movie_details(movie_title)
+            return JsonResponse(MovieSerializer(movie, context={'request': request}).data)
+        except OmdbApiResponseException:
+            return JsonResponse({'Error': 'Invalid movie title.'})
+        except Exception as ex:
+            logger.error('Unexpected error: {}'.format(ex))
+            return JsonResponse({'Error': 'Error occured, please try again later.'})
