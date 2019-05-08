40ddf624ec9f0a220158589022c976044ca34860
wojtek.przytarski <vojtek16@gmail.com>
2019-05-08 11:13:02 +0200

    heroku settings

--- movies_playground/settings.py
+++ movies_playground/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'movies_playground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
-#     "default":
-#         {
-#             # "ENGINE": "django.db.backends.postgresql_psycopg2",  # one of those should work
-#             'ENGINE': 'django.db.backends.postgresql',  # one of those should work
-#             "NAME": 'shielded-wave-76701',
-#             "HOST": "postgres://ossxeyneevltin:2ddfa05255fb83e138523c72afcebb4c7a1d375a0082b255237784964577802c@ec2-54-83-61-142.compute-1.amazonaws.com:5432/dcurkb6peqojpp",
-#             "PORT": "5432",
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+DATABASES = {
+    "default":
+        {
+            # "ENGINE": "django.db.backends.postgresql_psycopg2",  # one of those should work
+            'ENGINE': 'django.db.backends.postgresql',  # one of those should work
+            "NAME": 'shielded-wave-76701',
+            "HOST": "postgres://ossxeyneevltin:2ddfa05255fb83e138523c72afcebb4c7a1d375a0082b255237784964577802c@ec2-54-83-61-142.compute-1.amazonaws.com:5432/dcurkb6peqojpp",
+            "PORT": "5432",
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
